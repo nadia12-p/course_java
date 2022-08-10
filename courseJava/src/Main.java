10b27ac281ed87027da9ef5da4d57c11f53ff9ad
nadia12-p <nadiapintos98@gmail.com>
2022-08-10 13:23:08 -0500

    input stream reader

--- courseJava/src/Main.java
+++ courseJava/src/Main.java
@@ -1,10 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String nombre = "Nadia";
-        Integer followersInstagram = 2180;
-        String frase = "Hola mi nombre es: " + nombre + " tengo " + followersInstagram + " followers";
+        InputStreamReader capturarTeclado = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(capturarTeclado);
 
-        System.out.println(frase);
+        System.out.println("Ingrese su edad:");
+        String strEdad = buffer.readLine();
+
+        Integer edad = Integer.parseInt(strEdad);
+
+        if (edad >=18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
     }
 }
